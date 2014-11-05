e4c6377df65310c069c4079e616494d490ea3d7e
franzsch <franzschrader@gmx.net>
2014-11-05 15:11:54 +0100

    AD-5 - switched from hsql to h2 db - removed spring webflow module - adapted init sql file - added required repository-interfaces

--- src/main/resources/db/hsqldb/initDB.sql
+++ src/main/resources/db/hsqldb/initDB.sql
@@ -1,6 +1,7 @@
 DROP TABLE users IF EXISTS;
 DROP TABLE authorities IF EXISTS;
 DROP TABLE user_authorities IF EXISTS;
+DROP TABLE authority IF EXISTS;
 
 CREATE TABLE IF NOT EXISTS users (
  	 id 				INTEGER IDENTITY PRIMARY KEY,
@@ -27,74 +28,35 @@ CREATE TABLE IF NOT EXISTS authority (
 	name 				VARCHAR(60) NOT NULL
 );
 
+DROP TABLE spot IF EXISTS;
+DROP TABLE activity IF EXISTS;
+DROP TABLE advertisement IF EXISTS;
+DROP TABLE rating IF EXISTS;
+DROP TABLE activity IF EXISTS;
+DROP TABLE location IF EXISTS;
 
-DROP TABLE event IF EXISTS;
-DROP TABLE audio IF EXISTS;
-DROP TABLE catering IF EXISTS;
-
-CREATE TABLE IF NOT EXISTS event (
-  id         			INTEGER IDENTITY PRIMARY KEY,
-  name 		 			VARCHAR(60),
-  date 					datetime,
-  audio_id INTEGER NOT NULL,
-  catering_cateringId INTEGER NOT NULL,
-  light_lightId INTEGER NOT NULL,
-  location_locationId INTEGER NOT NULL,
-  rigging_riggingId INTEGER NOT NULL,
-  security_securityId INTEGER NOT NULL,
-  specialty_specialtyId INTEGER NOT NULL,
-  user_userId INTEGER NOT NULL
+CREATE TABLE spot(
+    ID INTEGER IDENTITY PRIMARY KEY,
+    NAME VARCHAR(255),
+    LOCATION_ID INTEGER,
+    USER_USERID INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS audio (
-	id					INTEGER IDENTITY PRIMARY KEY,
-	audioNeeded			TINYINT,
-	audioType			VARCHAR(100),
-	roomSize			INTEGER NOT NULL,
-	numberGuests		INTEGER NOT NULL	
+CREATE TABLE rating(
+    ID INTEGER IDENTITY PRIMARY KEY,
+    NAME VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS catering (
-	cateringId			INTEGER IDENTITY PRIMARY KEY,
-	eating 				TINYINT,
-	eatingType			VARCHAR(100),
-	eatingPeople		INTEGER NOT NULL,
-	drinking 			TINYINT,
-	drinkingPeople		INTEGER NOT NULL
+CREATE TABLE activity(
+    ID INTEGER IDENTITY PRIMARY KEY,
+    NAME VARCHAR(255),
+    RATING_ID INTEGER,
+    SPOT_SPOTID INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS light (
-	lightId				INTEGER IDENTITY PRIMARY KEY,
-	lightsNeeded 		TINYINT,
-	lightType			VARCHAR(100),
-	stageSize			INTEGER NOT NULL,
-	roomSize			INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS location (
-	locationId			INTEGER IDENTITY PRIMARY KEY,
-	name 				VARCHAR(100),
-	street				VARCHAR(100),
-	town				VARCHAR(100),
-	type				VARCHAR(100)
-);
-
-CREATE TABLE IF NOT EXISTS rigging (
-	riggingId			INTEGER IDENTITY PRIMARY KEY,
-	riggingNeeded		TINYINT,
-	riggingType			VARCHAR(100),
-	stageSize			VARCHAR(100)
-	
-);
-
-CREATE TABLE IF NOT EXISTS security (
-	securityId			INTEGER IDENTITY PRIMARY KEY,
-	securityNeeded 		TINYINT,
-	numberOfProtectees 	INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS specialty (
-	specialtyId			INTEGER IDENTITY PRIMARY KEY,
-	subject				VARCHAR(100),
-	comment				VARCHAR(100)
+CREATE TABLE advertisement(
+    ID INTEGER IDENTITY PRIMARY KEY,
+    NAME VARCHAR(255),
+    SPOT_SPOTID INTEGER,
+    USER_ID INTEGER
 );
