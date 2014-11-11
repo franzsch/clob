04cdfffa1bfff71444c26212830885f1aa73a7a3
Christian Henle <christian.henle@gmx.de>
2014-11-11 11:38:45 +0100

    AD-4 Example RestController added localhost:8080/series

---
+++ src/main/java/de/hs/furtwangen/bam/spots/controller/SeriesController.java
@@ -0,0 +1,27 @@
+package de.hs.furtwangen.bam.spots.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/series")
+public class SeriesController {
+	
+	
+	
+	
+	/**
+	 * Returns all registered series
+	 * @return
+	 * @throws InterruptedException 
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public Series[] getAllSeries() throws InterruptedException {
+		Thread.sleep(4000);	//pause to better show sync/async RestTemplate behavior
+		return new Series[]{
+				new Series(1, "The walking dead", "USA", "Thriller"), 
+				new Series(2, "Homeland", "USA", "Drama")};
+	}
+}
