b27f7a94946aa1de2701c9c04f693c220e9d8c66
Christian Henle <christian.henle@gmx.de>
2014-11-17 15:42:23 +0100

    AD-9 Der Privatkunde registriert eine separate Seite erstellt. Welche ausgetauscht werden kann.

--- src/main/webapp/html/app.js
+++ src/main/webapp/html/app.js
@@ -1,30 +1,31 @@
 (function(){
 
-  var app = angular.module('user',[ ]);
+  var app = angular.module('user',['ngRoute']);
+  
+  app.config(function ($routeProvider) {
+	    $routeProvider
+	        .when('/', {
+	            templateUrl: 'partials/register.html',
+	            controller: RegisterController
+	        })
+	        .otherwise({
+	            redirectTo: '/'
+	        });
+	});
+  
+  function RegisterController($scope, $location) {
+		
+	  this.user = {};
 
+      this.addUser = function(user)
+      {
+        this.user.passwordEquals = false;
 
-
-  app.directive('userRegister', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'user-register.html',
-      controller: function() {
-        this.user = {};
-
-        this.addUser = function(user)
+        if(angular.equals(user.password1, user.password2))
         {
-          this.user.passwordEquals = false;
-
-          if(angular.equals(user.password1, user.password2))
-          {
-            this.user = angular.copy(user);
-            this.user.passwordEquals = true;
-          }
-        };
-      },controllerAs: 'userCtrl'
-    };
-
-  });
-
-
+          this.user = angular.copy(user);
+          this.user.passwordEquals = true;
+        }
+      };
+	};
 })();
