62d37a7a578ee39cc65d35ba54d01b22436c8ff3
franzsch <franzschrader@gmx.net>
2014-12-16 12:04:52 +0100

    - added test location - added test ad location <-> spot and spot <-> ad consistency confirmed

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -1,3 +1,5 @@
 INSERT INTO USERS VALUES (1,'Christian','Henle','henlechr','80b181ffc3106d562a3b36e006ce6e67ede08fc2e39e6e226f961ff881fd5eb3dea0a52485220661','christian.henle@hs-furtwangen.de');
 INSERT INTO USERS VALUES (2,'Max','Testperson','Testperson2014','4b3135b78f1abfa05657df55abada39555e2c636ffe9f5b9523632b772d0e57db2015d93819acdf1','testperson@adventurespots.com');
 INSERT INTO SPOT VALUES (1, 'TestSpot', 1, 1);
+INSERT INTO LOCATION VALUES (1, 'TestLocation', 'Deutschland', 'TestStrasse', 'TestStadt', 'TestType', 1);
+INSERT INTO ADVERTISEMENT VALUES (1, 'TestAd', 1, 1);
