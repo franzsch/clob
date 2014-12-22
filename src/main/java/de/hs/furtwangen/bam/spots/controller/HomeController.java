59f7d60875fc3a1ab67ead2b1c647b0d8d60fd0f
Christian.Henle@gmx.de <Christian.Henle@gmx.de>
2014-12-22 15:14:13 +0100

    AD-55 Git Commit Plugin hinzufügen. Git Commit Plugin zeigt unter der URL /version an zu welchem Commit die vorliegende Softwareversion gehört.

--- src/main/java/de/hs/furtwangen/bam/spots/controller/HomeController.java
+++ src/main/java/de/hs/furtwangen/bam/spots/controller/HomeController.java
@@ -1,16 +1,59 @@
 package de.hs.furtwangen.bam.spots.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping
-    public String home() {
-        return "/app/index.html";
-    }
+	@Resource(name = "gitProperties")
+	private Properties gitProperties;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "/app/index.html";
+	}
+
+	@RequestMapping(value = "/version", method = RequestMethod.GET)
+	@ResponseBody
+	public List<PropertyBean> git() {
+		List<PropertyBean> listPropertyBean = new ArrayList<>();
+		for (String gitKey : gitProperties.stringPropertyNames()) {
+			PropertyBean propertyBean = new PropertyBean();
+			propertyBean.setKey(gitKey);
+			propertyBean.setValue(gitProperties.getProperty(gitKey));
+			listPropertyBean.add(propertyBean);
+		}
+		return listPropertyBean;
+	}
+
+	class PropertyBean {
+		String key;
+		String value;
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
 
 }
