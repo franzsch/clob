237b2660768782eb0841c6b164597d37d227df28
franzsch <franzschrader@gmx.net>
2015-02-28 01:34:02 +0100

    introduced location table for zip<->town mapping

--- src/main/resources/db/hsqldb/initDB.sql
+++ src/main/resources/db/hsqldb/initDB.sql
@@ -2,6 +2,7 @@ DROP TABLE users IF EXISTS;
 DROP TABLE authorities IF EXISTS;
 DROP TABLE user_authorities IF EXISTS;
 DROP TABLE authority IF EXISTS;
+DROP TABLE location IF EXISTS;
 
 CREATE TABLE IF NOT EXISTS users (
  	 id 				INTEGER IDENTITY PRIMARY KEY,
@@ -21,3 +22,12 @@ CREATE TABLE IF NOT EXISTS authority (
 	id 					INTEGER IDENTITY PRIMARY KEY,
 	name 				VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE location (
+  zip int(11) DEFAULT NULL,
+  ort varchar(255) DEFAULT NULL,
+  kreis varchar(255) DEFAULT NULL,
+  bundesland varchar(255) DEFAULT NULL,
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
